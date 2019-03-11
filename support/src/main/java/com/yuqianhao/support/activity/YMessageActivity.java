2bb9e2198d560f5b9f2cd94e0123f86770bfc1d8
马春燕 <1032528317@qq.com>
2019-03-11 15:56:48 +0800

    456

---
+++ support/src/main/java/com/yuqianhao/support/activity/YMessageActivity.java
@@ -0,0 +1,85 @@
+package com.yuqianhao.support.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.yuqianhao.support.activity.YBaseActivity;
+import com.yuqianhao.support.redpoint.*;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class YMessageActivity extends YBaseActivity implements  IRedPointCallback {
+    private Map<Integer, List<View>> viewMap = new HashMap<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RedPointManager.getInstance().register(this);
+        collectionRedPointView();
+    }
+    @Override
+    public void onResultRedPoint(List<RedPointBean> redPointBeanList) {
+        //从viewMap中循环出需要展示的view
+        List<View> showViews = new ArrayList<>();//需要展示的小红点
+        List<View> hindViews = new ArrayList<>();//需要隐藏的小红点
+        for (RedPointBean redPointBean : redPointBeanList) {
+            if (redPointBean.getRed_read() == 0) {//未读状态小红点
+                List<View> views = viewMap.get(redPointBean.getRed_type());
+                if (views != null) {
+                    showViews.addAll(views);
+                }
+            } else {//已读小红点
+                List<View> views = viewMap.get(redPointBean.getRed_type());
+                if (views != null) {
+                    hindViews.addAll(views);
+                }
+            }
+        }
+        ///!!!!必须先隐藏后展示,同一个view可能有多种状态会出问题
+        // 隐藏
+        for (View hindView : hindViews) {
+            hindView.setVisibility(View.INVISIBLE);
+        }
+        //展示
+        for (View showView : showViews) {
+            showView.setVisibility(View.VISIBLE);
+        }
+    }
+    //收集所有被注释小红点的控件
+    public void collectionRedPointView() {
+        Field[] fields = getClass().getDeclaredFields();//反射类的所有属性
+        if (fields != null) {
+            for (Field itemField : fields) {
+                RedPointForView annotation = itemField.getAnnotation(RedPointForView.class);//获取每个属性的注解
+                if (annotation != null) {
+                    int[] typeViews = annotation.typeView();//获取属性被注解的值
+                    try {
+                        View view = (View) itemField.get(this);//反射类获取view
+                        for (int type : typeViews) {//循环typeViews
+                            if (viewMap.containsKey(type)) {//包括此类型的小红点,直接添加view
+                                List<View> views = viewMap.get(type);
+                                views.add(view);
+                            } else {//添加一个新的类型
+                                List<View> views = new ArrayList<>();
+                                views.add(view);
+                                viewMap.put(type, views);
+                            }
+                        }
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RedPointManager.getInstance().unRegister(this);
+    }
+}
