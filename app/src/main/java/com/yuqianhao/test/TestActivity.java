444b2c55bba456709af8407d9f2e066112a6daa1
916640168@qq.com <yqh916640168****>
2019-03-14 06:57:19 +0800

    优化代码

--- app/src/main/java/com/yuqianhao/test/TestActivity.java
+++ app/src/main/java/com/yuqianhao/test/TestActivity.java
@@ -2,13 +2,7 @@ package com.yuqianhao.test;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.TextView;
 import com.yuqianhao.support.activity.YMessageActivity;
-import com.yuqianhao.support.redpoint.RedPointAnnotations;
-import com.yuqianhao.support.redpoint.RedPointView;
-import com.yuqianhao.support.redpoint.RedPointManager;
-import com.yuqianhao.support.redpoint.RedPointType;
 
 public class TestActivity extends YMessageActivity {
 
--- support/src/main/java/com/yuqianhao/support/redpoint/RedPointManager.java
+++ support/src/main/java/com/yuqianhao/support/redpoint/RedPointManager.java
@@ -21,6 +21,37 @@ public class RedPointManager {
     private static List<RedPointBean> redPointList = new ArrayList<>();//已存有数据的集合
     private List<IRedPointCallback> iRedPointCallbackList = new ArrayList<>();
 
+    /**
+     * 将通知消息对象插入到队列中
+     * @param redPointBean 变参，可以有多个对象
+     * */
+    public void putNotice(RedPointBean... redPointBean) {
+        for (RedPointBean redPointBean1 : redPointBean) {
+            redPointList.add(redPointBean1);
+        }
+    }
+
+    /**
+     * 将消息列表插入到消息队列中
+     * @param list 消息列表
+     * */
+    public void putNotice(List<RedPointBean> list) {
+        if (list != null) {
+            redPointList.addAll(list);
+        }
+    }
+
+    //点击小红点view
+    public void readNotice(int type) {
+        for (RedPointBean redPointBean : redPointList) {
+            if (type == redPointBean.getType()) {
+                redPointBean.setRead(1);//将未读的小红点设置为已读
+            }
+        }
+        for (IRedPointCallback iRedPointCallback : iRedPointCallbackList) {
+            iRedPointCallback.onResultRedPoint(redPointList);//通知界面刷新数据
+        }
+    }
 
     //A或者F注册绑定系小红点事件,并且获取需要显示的小红点的类型
     public void register(IRedPointCallback iRedPointCallback) {
@@ -37,16 +68,5 @@ public class RedPointManager {
 
     }
 
-    //点击小红点
-    public void readNotice(int type) {
-        for (RedPointBean redPointBean : redPointList) {
-            if (type == redPointBean.getType()) {
-                redPointBean.setRead(1);//将未读的小红点设置为已读
-            }
-        }
-        for (IRedPointCallback iRedPointCallback : iRedPointCallbackList) {
-            iRedPointCallback.onResultRedPoint(redPointList);//通知界面刷新数据
-        }
-    }
 
 }
